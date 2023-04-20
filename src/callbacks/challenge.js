95263da2d14c9708a173abb1b9df39a7477b7418
erobles <erobles@intergrupo.com>
2023-04-20 09:46:54 -0500

    fin de lección de callbacks

--- src/callbacks/challenge.js
+++ src/callbacks/challenge.js
@@ -1,11 +1,11 @@
 // instancio el request
-const xmlHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 //inicio la API en la raíz
-const API = 'https://api.escuelajs.co/api/v1/'
+const API = 'https://api.escuelajs.co/api/v1'
 
 function fechtData(urlApi, callback) {
     let xhttp = new XMLHttpRequest();
-    xhttp.open(get, urlApi, true);
+    xhttp.open('GET', urlApi, true);
     xhttp.onreadystatechange = function (event) {
         /* 
             * 0 -> no inicializado
@@ -19,33 +19,31 @@ function fechtData(urlApi, callback) {
 
             if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
-            }
-        } else {
-            const error = new Error('Error ' + urlApi);
-        }
-    }
-    xhttp.send();
-}
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const API = 'https://api.escuelajs.co/api/v1';
-
-function fechtData(urlAPI, callback) {
-    let xhttp = new XMLHttpRequest();
-
-    xhttp.open('GET', urlAPI, true);
-    xhttp.onreadystatechange = function (event) {
-        if (xhttp.readyState === 4) {
-            //valores 0: No se inicializado, 1:,2:, 3:, 4:
-            if (xhttp.status === 200) {
-                callback(null, JSON.parse(xhttp.responseText));
             } else {
-                const error = new Error('Error ' + urlAPI);
+                const error = new Error('Error ' + urlApi);
                 return callback(error, null);
             }
-
         }
     }
-
     xhttp.send();
 }
 
+fechtData(`${API}/products`,
+    function (error1, data1) {
+        if (error1) return console.error(error1);
+        fechtData(`${API}/products/${data1[0].id}`,
+
+            function (error2, data2) {
+                if (error2) return console.error(error2);
+
+                fechtData(`${API}/categories/${data2.category?.id}`, function (error3, data3) {
+                    if (error3) return console.error(error3);
+                    console.log(data1[0]);
+                    console.log(data2.title);
+                    console.log(data3.name);
+                });
+
+
+            });
+
+    });
